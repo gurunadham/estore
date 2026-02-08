cf8925c9d7128a006c58a9ed6ef839a22a143797
gurunadham <45356000+gurunadham@users.noreply.github.com>
2026-02-08 14:00:41 +0530

    search bar changes in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  faSearch = faSearch;
 }
