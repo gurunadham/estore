88b7da621f1ccf3e5889cb8ac692a5080bf572b6
gurunadham <45356000+gurunadham@users.noreply.github.com>
2026-02-08 14:44:26 +0530

    adding login link whislist and cart icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faUserCircle, faHeart, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +10,7 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   faSearch = faSearch;
+  faUserCircle = faUserCircle;
+  faHeart = faHeart;
+  faShoppingCart = faShoppingCart;
 }
