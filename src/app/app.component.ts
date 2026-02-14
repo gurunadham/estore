1b2f41f7014c2069df5034c74618778aa2f9b1b1
gurunadham <45356000+gurunadham@users.noreply.github.com>
2026-02-14 10:45:36 +0530

    initial home components changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { CatnavigationComponent } from './components/home/catnavigation/catnavigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent, CatnavigationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
