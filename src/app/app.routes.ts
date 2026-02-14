b1fe8f4c7fb66f25cd8cdd3acdb476a3840b8093
gurunadham <45356000+gurunadham@users.noreply.github.com>
2026-02-14 11:34:41 +0530

    feature for page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { PageNotComponentComponent } from './components/page-not-component/page-not-component.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: '**', component: PageNotComponentComponent}
+    {path: '**', component: PageNotFoundComponent}
 ];
